b9dbf6333da460ccf70c972a44cad908dc9c0598
Srikar-Sharma <sadhusrikarsharma@outlook.com>
2023-02-17 15:37:51 +0530

    Sadu

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,7 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello just checking if this is working or not");
     }
 
     // Update is called once per frame
