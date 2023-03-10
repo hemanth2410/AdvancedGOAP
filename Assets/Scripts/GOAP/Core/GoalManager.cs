77f4d452b347ed67a6f715de2a4bb882cf907873
Sai Hemanth <saihemanth.s@outlook.com>
2023-03-10 15:40:30 +0530

    Very crude Npc Goal Manager ToDo : Test it with creating various goals

--- Assets/Scripts/GOAP/Core/GoalManager.cs
+++ Assets/Scripts/GOAP/Core/GoalManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GoalManager : MonoBehaviour
 {
-    [SerializeField] NpcGoals m_NpcGoals;
+    [SerializeField] List<NpcGoalData> m_NpcGoals;
+    NpcGoalData _NpcGoalData;
+    public List<NpcGoalData> NpcGoals { get { return m_NpcGoals; } }
+    public NpcGoalData NpcGoalData { get { return _NpcGoalData; } }
     //This class should be able to dynamically adjust the goal on priority
     //and needs to make sure that only one goal comes out as Highest priority at any given time
     //goal's priority can be dependant on external events or internal events.
@@ -19,4 +23,15 @@ public class GoalManager : MonoBehaviour
     {
         
     }
+
+    private void FixedUpdate()
+    {
+        foreach(NpcGoalData npcGoalData in m_NpcGoals)
+        {
+            npcGoalData.evaluatePriority();
+        }
+        m_NpcGoals.OrderByDescending(x => x.GoalPriority);
+        _NpcGoalData = m_NpcGoals.First();
+    }
+
 }
