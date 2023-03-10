7f0a01031e76f3d42042dc6f96577d1967125c1a
Sai Hemanth <saihemanth.s@outlook.com>
2023-03-10 14:54:47 +0530

    GoalManager

---
+++ Assets/Scripts/GOAP/Core/GoalManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalManager : MonoBehaviour
+{
+
+    //This class should be able to dynamically adjust the goal on priority
+    //and needs to make sure that only one goal comes out as Highest priority at any given time
+    //goal's priority can be dependant on external events or internal events.
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
