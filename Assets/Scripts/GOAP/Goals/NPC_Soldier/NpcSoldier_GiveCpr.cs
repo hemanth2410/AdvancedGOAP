1ff0cb9502cf919a5c307b61e468edbdde4fba0a
Sai Hemanth <saihemanth.s@outlook.com>
2023-03-28 17:54:49 +0530

    Dev log 28/3

--- Assets/Scripts/GOAP/Goals/NPC_Soldier/NpcSoldier_GiveCpr.cs
+++ Assets/Scripts/GOAP/Goals/NPC_Soldier/NpcSoldier_GiveCpr.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class NpcSoldier_GiveCpr : NpcGoalData
 {
+    [SerializeField] float m_detectionRadius;
+    public override void GetBelieves()
+    {
+        base.GetBelieves();
+    }
     public override void evaluatePriority()
     {
         base.evaluatePriority();
+        // Physics.OverLap sphere and get the colliders and check if NPC is wounded.
+        // if so then inject that belief into NPC
     }
 }
